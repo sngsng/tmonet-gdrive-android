4107b3e3e56cd3c218b6c64e876f903b39d735c6
Jessehj <jessehj@slogup.com>
2017-06-07 17:47:33 +0900

    feat : SearchView layout

--- app/src/main/java/kr/co/tmonet/gdrive/controller/activity/MapActivity.java
+++ app/src/main/java/kr/co/tmonet/gdrive/controller/activity/MapActivity.java
@@ -1,6 +1,7 @@
 package kr.co.tmonet.gdrive.controller.activity;
 
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,11 +15,23 @@ public class MapActivity extends TMapBaseActivity implements ChargeListDialogFra
     private static final String LOG_TAG = MapActivity.class.getSimpleName();
 
     private ActivityMapBinding mBinding;
+    private View mDecorView;
+    private int mUiOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_map);
+
+        mDecorView = getWindow().getDecorView();
+        mUiOption = getWindow().getDecorView().getSystemUiVisibility();
+        mUiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            mUiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            mUiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        mDecorView.setSystemUiVisibility(mUiOption);
 
         setUpViews();
         setUpActions();
@@ -36,15 +49,31 @@ public class MapActivity extends TMapBaseActivity implements ChargeListDialogFra
         });
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            mDecorView.setSystemUiVisibility(mUiOption);
+        }
+    }
+
     private void setUpViews() {
 
     }
 
     private void setUpActions() {
-        mBinding.searchTextView.setOnClickListener(new View.OnClickListener() {
+        mBinding.search.beforeSearchLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showSearchAddressDialog();
+//                showSearchAddressDialog();
+                mBinding.search.beforeSearchLayout.setVisibility(View.GONE);
+                mBinding.search.afterSearchLayout.setVisibility(View.VISIBLE);
+            }
+        });
+        mBinding.search.searchClearImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBinding.search.beforeSearchLayout.setVisibility(View.VISIBLE);
+                mBinding.search.afterSearchLayout.setVisibility(View.GONE);
             }
         });
         mBinding.chargeStationImageView.setOnClickListener(new View.OnClickListener() {
